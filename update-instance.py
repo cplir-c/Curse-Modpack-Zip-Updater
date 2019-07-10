ce15024801869f26ad9635043f6c1970059d61df
cplir-c <petemclinn@gmail.com>
2019-07-10 15:31:10 -0500

    Fix keyerror when the mod manifest was unchanged

--- update-instance.py
+++ update-instance.py
@@ -13,6 +13,19 @@ This module updates your multimc modpack.
 It checks the difference between the old zip file and the new zip file, and changes the new zip file to make an updated instance when installed.
 
 Line-level diffing is supported.
+
+New plan:
+    Copy the newer zip file, and put overrides in there.
+    Copy the files from the instance to the new zip file if 
+      they aren't the same as the ones in the overrides in the old zip file.
+    Exclude mods in the old zip file's manifest from copying.
+    Optionally copy all mods that aren't in the new zip file's manifest to
+      the new zip file and remove the mods listed in the new zip's manifest
+      that are already downloaded.
+    
+    Copy the newer zip file to a new zip file.
+    Compare the files from the instance folder and the old zip file's overrides folder.
+    Copy files in the instance folder that 
 '''
 class List(list):
     "This class extends the built-in list class to support hashing by locking itself when hashed."
@@ -80,7 +93,7 @@ def check_zipfile(zip_file):
         print(zip_file,str(zip_file))
         raise FileNotFoundError(str(zip_file))
 
-def get_files(instance_folder, zip_file_folder):
+def get_files(instance_folder, zip_file_folder, zip_new = None):
     "This function gets the zip files for updating, and the instance folder path."
     working_dir = Path.cwd()
     instance_folder = Path(working_dir,instance_folder).resolve()
@@ -105,14 +118,16 @@ def get_files(instance_folder, zip_file_folder):
     folder_contents = [check_zipfile(file) or file for file in zip_file_folder.iterdir()]
     folder_contents.sort(key=difference_key,reverse=False)
     zip_names = [file.name for file in folder_contents]
-
-    print('Zip files available:')
-    i = None #To ensure it's visible after the loop
-    for i,zip_new in enumerate(folder_contents):
-        print(str(i)+')',zip_new)
+    
+    i=len(folder_contents)
+    print(zip_new)
+    if zip_new is not None:
+        zip_new = str(zip_new)
+    while zip_new not in (list(map(str,range(i)))+zip_names):
+        print('Zip files available:')
+        for i,zip_new in enumerate(folder_contents):
+            print(str(i)+')',zip_new)
         
-    zip_new = None
-    while zip_new not in (list(map(str,range(i+1)))+zip_names):
         zip_new = input('Zip file to update to? ')
     
     if zip_new in zip_names:
@@ -120,16 +135,17 @@ def get_files(instance_folder, zip_file_folder):
     else:
         zip_new = int(zip_new)
     zip_new = folder_contents[zip_new]
-    print("You selected",zip_new)
+    print("You selected",zip_new.name)
     sleep(1)
     check_zipfile(zip_new)
 
     return (instance_folder,zip_old,zip_new)
 
-def inspect_files(old_zip, new_zip, in_both):
+def inspect_files(old_zip:ZipFile, new_zip:ZipFile, in_both:frozenset):
     old_contents = List(map(old_zip.getinfo,in_both))
     new_contents = List(map(new_zip.getinfo,in_both))
-
+    
+    print(old_zip.filename,new_zip.filename)
     contents_to_ignore = set() #For clarity
 
     lines_to_delete = default_dict(List)
@@ -157,6 +173,8 @@ def inspect_files(old_zip, new_zip, in_both):
         old_contents = [str(line)[2:-1] for line in old_contents]
         new_contents = [str(line)[2:-1] for line in new_contents]
         diff = List(Differ().compare(old_contents, new_contents))
+        print(diff)
+        print(old_contents)
         print('inspected', name)
         #try:
         #    #print(*diff,sep='\n',end='\n\n')
@@ -171,12 +189,14 @@ def inspect_files(old_zip, new_zip, in_both):
                 lines_to_delete[name].append(line[2:])
                 #try:print(line)
                 #except UnicodeEncodeError: pass
-            elif line.startswith('? '): pass
+            #elif line.startswith('? '): pass
                 #ignore this line
-            else: pass
+            #else: pass
                 #Also ignore this line
                 
         #sleep(0.2)
+    print(len(contents_to_ignore))
+
     return (lines_to_delete,lines_to_add)
 
 def compare_zipfile_contents(zip_old, zip_new):
@@ -190,33 +210,41 @@ def compare_zipfile_contents(zip_old, zip_new):
     #Old files
     contents_to_delete = old_zip_contents - new_zip_contents
     assert not (contents_to_delete & new_zip_contents)
-    assert contents_to_delete < old_zip_contents
+    print(len(contents_to_delete),len(old_zip_contents))
+    assert contents_to_delete <= old_zip_contents
     #New files
     contents_to_add = new_zip_contents - old_zip_contents
     assert not (contents_to_add & old_zip_contents)
-    assert contents_to_add < new_zip_contents
+    print(len(contents_to_add),len(new_zip_contents))
+    assert contents_to_add <= new_zip_contents
     #Interesting files
     contents_to_inspect = old_zip_contents & new_zip_contents
 
     (lines_to_delete,lines_to_add) = inspect_files(zip_old, zip_new, contents_to_inspect)
     return contents_to_delete, contents_to_add, lines_to_delete, lines_to_add
 
-def update_from_zip(instance_folder, zip_file_folder):
+def update_from_zip(instance_folder, zip_file_folder, new_zip = None):
     "The main function. Updates the given instance using the zips in the folder. Both are string paths."
-    instance_folder, zip_old_path, zip_new_path = get_files(instance_folder, zip_file_folder)
+    instance_folder, zip_old_path, zip_new_path = get_files(instance_folder, zip_file_folder, new_zip)
     zip_old,zip_new = map(ZipFile,map(str,(zip_old_path, zip_new_path)))
 
     (files_to_delete, files_to_add, lines_to_delete, lines_to_add) = compare_zipfile_contents(zip_old, zip_new)
 
-    print(*files_to_delete,sep='\n',end='\n\n')
-
+    if not any((files_to_delete, files_to_add, lines_to_delete, lines_to_add)):
+        raise ValueError("The zip files you selected: "+zip_old_path+", and "+zip_new_path+" were found to be the same. Please don't try to update twice!")
+    
+    #print(*files_to_delete,sep='\n',end='\n\n')
+    any(map(pprint,(lines_to_delete,lines_to_add)))
     #mods_to_delete = lines_to_delete['manifest.json']
     #mods_to_download = lines_to_add['manifest.json']
     #pprint(mods_to_delete)
     #pprint(mods_to_download)
-    del lines_to_delete['manifest.json'],lines_to_add['manifest.json']
+    if lines_to_delete:
+        del lines_to_delete['manifest.json']
+    if lines_to_add:
+        del lines_to_add['manifest.json']
     #It's going to be in the new zip file anyway.
     
 update_from_zip(r'..\..\..\mc\mmc-stable-win32\MultiMC\instances\ATM3-_Expert_Shedding_Tiers-1.1.4',
-                r'..\..\..\mc\mmc-stable-win32\MultiMC\instances\expert')
+                r'..\..\..\mc\mmc-stable-win32\MultiMC\instances\expert', 4)
     
